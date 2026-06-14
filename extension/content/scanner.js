be824657b8b599bd771db813cfe48d1d31163b62
nehakb07 <nehakb07@gmail.com>
2026-06-14 14:03:04 +0530

    updated Chrome extension structure

--- extension/content/scanner.js
+++ extension/content/scanner.js
@@ -1,3 +1,32 @@
 console.log(
     "Job Autofiller Loaded"
 );
+chrome.runtime.onMessage.addListener(
+  async (message) => {
+
+    if (message.action !== "autofill")
+      return;
+
+    const data =
+      await chrome.storage.local.get(
+        "profile"
+      );
+
+    console.log(
+      "PROFILE",
+      data.profile
+    );
+});
+chrome.runtime.onMessage.addListener(
+  async (message) => {
+
+    console.log("MESSAGE RECEIVED:", message);
+
+    if (message.action !== "autofill") {
+      return;
+    }
+
+    const data = await chrome.storage.local.get("profile");
+
+    console.log("PROFILE:", data.profile);
+});
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -3,14 +3,63 @@ document
 .addEventListener("click", async () => {
 
     const profile = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value
+        personal: {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            address: ""
+        },
+
+        education: {
+            college: "",
+            university: "",
+            cgpa: ""
+        },
+
+        links: {
+            linkedin: "",
+            github: "",
+            portfolio: ""
+        }
     };
 
     await chrome.storage.local.set({
         profile
     });
 
+    console.log("PROFILE SAVED", profile);
+
     alert("Profile Saved");
+});
+document
+.getElementById("autofillBtn")
+.addEventListener("click", async () => {
+
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+
+    try {
+
+    const response =
+        await chrome.tabs.sendMessage(
+            tab.id,
+            {
+                action: "autofill"
+            }
+        );
+
+    console.log(response);
+
+}
+catch(error){
+
+    console.error(
+        "MESSAGE ERROR:",
+        error
+    );
+
+}
+
 });
